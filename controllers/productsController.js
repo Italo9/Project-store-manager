44bf3c8b7e2674ee3ab06878fcf1372454d79de6
ítalo Lima <contatoitallo@hotmail.com>
2022-06-29 15:53:02 -0300

    chore: Implementa parcialmente arquitetura MSC: Camada controller estruturada

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,2 +1,36 @@
 const productsService = require('../services/productsService');
 
+const httpStatus = require('../helpers/httpStatusCode');
+
+const getAll = async (req, res) => {
+  try {
+    const results = await productsService.getAll();
+    if (!results) {
+      return res.status(httpStatus.NOT_FOUND).json({ message: 'Product not found' });
+    }
+    res.status(httpStatus.OK).json(results);
+  } catch (err) {
+    console.error(err);
+    res.status(httpStatus.INTERNAL_SERVER).json({ message: 'Erro ao tentar realizar operação' });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await productsService.getById(id);
+    if (!result || result.length < 1) {
+      return res.status(httpStatus.NOT_FOUND).json({ message: 'Product not found' });
+    }
+    res.status(httpStatus.OK).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(httpStatus.INTERNAL_SERVER).send('Erro ao tentar realizar operação');
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
