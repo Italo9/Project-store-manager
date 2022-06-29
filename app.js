2753b08a42f08aa53a1d64822280ce5ec74d536c
ítalo Lima <contatoitallo@hotmail.com>
2022-06-29 15:46:11 -0300

    chore: Implementa parcialmente arquitetura MSC Inicialmente foi criado o controller a pasta helpers com um arquivo de status http e o service

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const rescue = require('express-rescue');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const router = require('./routers/index');
 
 const app = express();
 
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+app.use('/products', rescue(router.productsRouter));
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
@@ -10,5 +18,4 @@ app.get('/', (_request, response) => {
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
-// c
 module.exports = app;
