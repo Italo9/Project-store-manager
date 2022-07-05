534d15247ee5a7a8db0952e9381c06d5851923a7
ítalo Lima <contatoitallo@hotmail.com>
2022-07-05 18:39:32 -0300

    chore: Implementa teste na camada service

--- tests/unit/productsService.test.js
+++ tests/unit/productsService.test.js
@@ -19,7 +19,6 @@ describe('Obtém todos os dados de productsModel', () => {
       }
     ];
     sinon.stub(productsModel, 'getAll').resolves(mockPayloadProducts)
-    sinon.stub(productsModel, 'getById').resolves(mockPayloadProducts[0])
   })
 
   after(() => {
@@ -29,8 +28,43 @@ describe('Obtém todos os dados de productsModel', () => {
     const result = await productsService.getAll()
     expect(result).to.be.a('array')
   })
+})
+
+describe('Obtém todos os dados de productsModel', () => {
+
+  before(() => {
+    const mockPayloadProducts = {
+      "id": 1,
+      "name": "Martelo de Thor",
+    };
+    sinon.stub(productsModel, 'getById').resolves(mockPayloadProducts)
+  })
+
+  after(() => {
+    sinon.restore()
+  })
+
   it('Retorna um objeto que representa um produto', async () => {
     const result = await productsService.getById(1)
     expect(result).to.be.a('object')
   })
 })
+
+// describe('Obtém todos os dados de salesModel', () => {
+
+//   before(() => {
+//     const mockPayloadSales = {
+//       id: 1,
+//       name: 'teste',
+//     };
+//     sinon.stub(salesModel, 'update').resolves(mockPayloadSales)
+//   })
+
+//   after(() => {
+//     sinon.restore()
+//   })
+//   it('Retorna um object com informações atualizadas', async () => {
+//     const result = await salesService.update(1, 'xablau')
+//     expect(result).to.be.a('object')
+//   })
+// })
--- tests/unit/salesService.test.js
+++ tests/unit/salesService.test.js
@@ -9,28 +9,61 @@ describe('Obtém todos os dados de salesModel', () => {
   before(() => {
     const mockPayloadSales = [
       {
-        "productId": 1,
-        "quantity": 1
+        saleId: 1,
+        date: "2021-09-09T04:54:29.000Z",
+        productId: 1,
+        quantity: 2
       },
-      {
-        "productId": 2,
-        "quantity": 5
-      }
     ];
     sinon.stub(salesModel, 'getAll').resolves(mockPayloadSales)
+  })
+
+  after(() => {
+    sinon.restore()
+  })
+  it('Verificar se é um array de retorno', async () => {
+    const result = await salesService.getAll()
+    expect(result).to.be.a('array')
+  })
+})
+
+describe('Obtém todos os dados de salesModel', () => {
+
+  before(() => {
+    const mockPayloadSales = [
+      {
+        date: "2021-09-09T04:54:29.000Z",
+        productId: 1,
+        quantity: 2
+      },
+    ];
     sinon.stub(salesModel, 'getById').resolves(mockPayloadSales)
   })
 
   after(() => {
     sinon.restore()
   })
-  //t
-  it('Verificar se é um array de retorno', async () => {
-    const result = await salesService.getAll()
-    expect(result).to.be.a('array')
-  })
   it('Retorna um array com informações de venda', async () => {
     const result = await salesService.getById(1)
     expect(result).to.be.a('array')
+  })
+})
+
+describe('Obtém todos os dados de salesModel', () => {
+
+  before(() => {
+    const mockPayloadSales = {
+      saleId: 1,
+      itemsUpdated: [],
+    };
+    sinon.stub(salesModel, 'update').resolves(mockPayloadSales)
+  })
+
+  after(() => {
+    sinon.restore()
+  })
+  it('Retorna um object com informações atualizadas', async () => {
+    const result = await salesService.update(1, [])
+    expect(result).to.be.a('object')
   })
 })
